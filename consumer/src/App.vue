07e4e87d246a04e15415bf1da999e4122dab0f92
zj9495 <zj9495@gmail.com>
2021-10-09 16:10:24 +0800

    working with 5.0.0-beta.3

---
+++ consumer/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    This is /consumer.
+
+    <hr />
+    <h4>The content below is imported from /core</h4>
+    <Section>
+      <Button>Button imported from /core</Button>
+      <br />
+      I'm inside the Section component loaded from /core, but my text is defined
+      in /consumer
+    </Section>
+
+    <hr />
+    <h4>The content below is imported from /other</h4>
+
+    <MainComponent />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {
+    Button: () => import("core/Button"),
+    Section: () => import("core/Section"),
+    MainComponent: () => import("other/MainComponent"),
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ core/vue.config.js
@@ -0,0 +1,22 @@
+const ModuleFederationPlugin =
+  require("webpack").container.ModuleFederationPlugin;
+
+module.exports = {
+  publicPath: "http://localhost:9000/",
+  configureWebpack: {
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "core",
+        filename: "remoteEntry.js",
+        exposes: {
+          "./Button": "./src/components/Button",
+          "./Section": "./src/components/Section",
+        },
+        shared: require("./package.json").dependencies,
+      }),
+    ],
+  },
+  devServer: {
+    port: 9000,
+  },
+};
